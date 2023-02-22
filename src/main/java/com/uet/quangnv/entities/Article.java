7f722494dc3da7dfa78e4ed75d7368d2b5981268
QuangNV1609 <71381696+QuangNV1609@users.noreply.github.com>
2023-02-22 21:52:51 +0700

    udp

--- src/main/java/com/uet/quangnv/entities/Article.java
+++ src/main/java/com/uet/quangnv/entities/Article.java
@@ -4,13 +4,14 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "article")
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
     @Id
     private String id;
