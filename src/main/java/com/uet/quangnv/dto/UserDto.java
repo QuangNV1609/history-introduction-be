825ef5eb640064b3184b240f342e72f845681e96
QuangNV1609 <71381696+QuangNV1609@users.noreply.github.com>
2023-04-26 20:56:30 +0700

    udp

--- src/main/java/com/uet/quangnv/dto/UserDto.java
+++ src/main/java/com/uet/quangnv/dto/UserDto.java
@@ -37,4 +37,36 @@ public class UserDto {
     public void setRoleName(List<String> roleName) {
         this.roleName = roleName;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public Long getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(Long imageId) {
+        this.imageId = imageId;
+    }
 }
